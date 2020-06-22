2dfa20c2dca5dcad690cf708cd87ab14bb320a70
Thomas de Graaff <t.de.graaff@vu.nl>
2020-06-22 15:16:33 +0200

    Change that damn owl!

---
+++ .auctex-auto/datasharing.el
@@ -0,0 +1,39 @@
+(TeX-add-style-hook
+ "datasharing"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "ignorenonframetext")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("babel" "english")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "amssymb"
+    "amsmath"
+    "fontenc"
+    "inputenc"
+    "microtype"
+    "longtable"
+    "booktabs"
+    "babel"
+    "pgf"
+    "times"
+    "etex"
+    "tcolorbox")
+   (LaTeX-add-xcolor-definecolors
+    "links"
+    "bashcodebg")
+   (LaTeX-add-tcbuselibraries
+    "minted,skins"))
+ :latex)
+
